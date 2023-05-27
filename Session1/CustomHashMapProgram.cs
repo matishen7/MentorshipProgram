37cfedb62d0dd873202d6f62488b1176e6eb0bf7
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-05-27 12:39:39 -0500

    my map

--- Session1/CustomHashMapProgram.cs
+++ Session1/CustomHashMapProgram.cs
@@ -37,7 +37,17 @@ namespace MentorshipProgram.Session1
             var mymap = new MyHashMap<string, string>();
             mymap.Add("key1", "val1");
             mymap.Add("key2", "val2");
+            mymap.Add("key2", "val3");
             mymap.Add("key3", "val3");
+            mymap.Add("key4", "val4");
+            //mymap.Add("key3", "val3");
+            // mymap.Add("key3", "val3");
+            //mymap.Add("key3", "val3");
+            Console.WriteLine(mymap.Get("key1"));
+            //Console.WriteLine(mymap.Get("key2"));
+            //Console.WriteLine(mymap.Get("key4"));
+            //Console.WriteLine(mymap.Get("key5"));
+            //Console.WriteLine(mymap.Get("key6"));
 
         }
 
@@ -46,56 +56,91 @@ namespace MentorshipProgram.Session1
             private TKey Key { get; set; }
             private TValue Value { get; set; }
 
-            private TValue[] Elements { get; set; }
+            private Node<TKey, TValue>[] Elements { get; set; }
 
-            private int size = 2;
+            private int size = 1;
 
             private int currentIndex = 0;
 
 
             public MyHashMap(int size)
             {
-                Elements = new TValue[size];
+                Elements = new Node<TKey, TValue>[size];
             }
 
             public MyHashMap()
             {
-                Elements = new TValue[size];
+                Elements = new Node<TKey, TValue>[size];
+            }
+
+            public void Put(TKey key, TValue value)
+            {
+                int hashcode = key.GetHashCode();
+                int index = Math.Abs(hashcode) % Elements.Length;
+                // Elements[index] = value;
+            }
+
+            public TValue Get(TKey key)
+            {
+                int hashcode = key.GetHashCode();
+                int index = Math.Abs(hashcode) % Elements.Length;
+                var node = findKey(key, Elements[index]);
+                return (node != null) ? node.value : default(TValue);
             }
 
             public void Add(TKey key, TValue value)
             {
                 int hashcode = key.GetHashCode();
+                int index = Math.Abs(hashcode) % Elements.Length;
+                var node = new Node<TKey, TValue>(key, value, hashcode);
                 if (currentIndex < Elements.Length)
                 {
-                    int index = Math.Abs(hashcode) % Elements.Length;
-                    Elements[index] = value;
+                    if (Elements[index] == null)
+                        Elements[index] = node;
+                    else
+                    {
+                        var head = Elements[index];
+                        var exisingNode = findKey(key, head);
+                        if (exisingNode == null)
+                            head.next = node;
+                    }
                 }
                 else
                 {
-
-                    var newElements = new TValue[size * 2];
-                    Array.Copy(Elements, newElements, size);
-                    int index = Math.Abs(hashcode) % newElements.Length;
-                    newElements[index] = value;
+                    var newElements = new Node<TKey, TValue>[Elements.Length * 2];
+                    Array.Copy(Elements, newElements, Elements.Length);
+                    index = Math.Abs(hashcode) % newElements.Length;
+                    if (newElements[index] == null)
+                        newElements[index] = node;
                     Elements = newElements;
                 }
                 currentIndex++;
+            }
+
+            private Node<TKey, TValue> findKey(TKey key, Node<TKey, TValue> head)
+            {
+                var current = head;
+                while (current != null)
+                {
+                    if (current.key.Equals(key)) return current;
+                    current = current.next;
+                }
+                return null;
             }
         }
 
         private class Node<TKey, TValue>
         {
             int hashCode;
-            TValue value;
-            TKey key;
-            Node<TKey, TValue> next;
+            public TValue value;
+            public TKey key;
+            public Node<TKey, TValue> next;
 
-            public Node(TKey key, TValue value)
+            public Node(TKey key, TValue value, int hashCode)
             {
                 this.key = key;
                 this.value = value;
-
+                this.hashCode = hashCode;
             }
         }
     }
