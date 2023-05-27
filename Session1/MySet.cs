1c033bb7a2ceef0269f8c82e433ef63e41f399bb
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-05-26 21:02:49 -0500

    MySetProgram

---
+++ Session1/MySet.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace MentorshipProgram.Session1
+{
+    [TestClass]
+    public class MySetProgram
+    {
+        [TestMethod]
+        public void test_my_set()
+        {
+            var set1 = new MySet<string>();
+            set1.Add("a");
+            set1.Add("b");
+            set1.Add("c");
+            set1.Add("c");
+            set1.Remove("a");
+        }
+
+        public class MySet<T>
+        {
+            Dictionary<T, Object> keys;
+            public MySet()
+            {
+                keys = new Dictionary<T, Object>();
+            }
+
+            public void Add(T key)
+            {
+                keys[key] = new object();
+            }
+
+            public void Remove(T key)
+            {
+                keys.Remove(key);
+            }
+        }
+    }
+}
