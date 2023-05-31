aebfbb4274faa03be7aa458186773590ffc18a1a
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-05-31 16:12:15 -0500

    HeapProgram

--- Session2/HeapProgram.cs
+++ Session2/HeapProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Reflection;
 
 namespace MentorshipProgram.Session2
 {
@@ -13,10 +14,31 @@ namespace MentorshipProgram.Session2
 
         public class MyHeap
         {
-
+            private int[] elements = new int[] { };
+            private int index = 0;
             public MyHeap()
             {
+                elements = new int[] { };
+            }
 
+            public MyHeap(int[] elements)
+            {
+                this.elements = elements;
+                index = elements.Length;
+            }
+
+            public MyHeap(int size)
+            {
+                elements = new int[size];
+            }
+
+            public void Push(int value)
+            {
+                if (index >= elements.Length)
+                {
+                    Resize();
+                }
+                elements[index++] = value;
             }
         }
     }
