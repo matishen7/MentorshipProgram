3490bc2b2f3f03d3ab9f7846bf61f3130028f63d
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-04 21:33:08 -0500

    SingletonDesignPatternProgram

---
+++ Session3/SingletonDesignPatternProgram.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MentorshipProgram.Session3
+{
+    [TestClass]
+    public class SingletonDesignPatternProgram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var eager_singleton = SingletonEager.GetInstance();
+            Console.WriteLine(eager_singleton.GetHashCode()); // hashcode is equal, because it is the same instance 
+
+            var eager_singleton2 = SingletonEager.GetInstance();
+            Console.WriteLine(eager_singleton2.GetHashCode());
+
+            var lazy_singleton = SingletonLazy.GetInstance();
+            Console.WriteLine(lazy_singleton.GetHashCode());// hashcode is equal, because it is the same instance
+
+            var lazy_singleton2 = SingletonLazy.GetInstance();
+            Console.WriteLine(lazy_singleton2.GetHashCode());
+        }
+
+        public class SingletonEager
+        {
+            private static SingletonEager instance = new SingletonEager();
+            private SingletonEager()
+            {
+
+            }
+
+            public static SingletonEager GetInstance()
+            {
+                return instance;
+            }
+
+        }
+
+        public class SingletonLazy
+        {
+            private static SingletonLazy instance = new SingletonLazy();
+            private SingletonLazy()
+            {
+
+            }
+
+            public static SingletonLazy GetInstance()
+            {
+                return instance;
+            }
+
+        }
+    }
+}
