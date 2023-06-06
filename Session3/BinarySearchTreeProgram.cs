a2907b4b3a1a279a2e3ac33630f37ed5bc9b36ab
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-06 18:06:19 -0500

    BinaryNode

---
+++ Session3/BinarySearchTreeProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MentorshipProgram.Session3
+{
+    [TestClass]
+    public class BinarySearchTreeProgram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        public class BinaryNode
+        {
+            public int value;
+            public BinaryNode left;
+            public BinaryNode right;
+
+            public BinaryNode() { }
+            public BinaryNode(int value) { this.value = value; }
+
+            public BinaryNode(int value, BinaryNode left, BinaryNode right) : this(value)
+            {
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+    }
+}
+
