bb441eeefad36ff042bf1626e652b2f5c72777d9
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-07 22:23:55 -0500

    BinarySearchTree

--- Session3/BinarySearchTreeProgram.cs
+++ Session3/BinarySearchTreeProgram.cs
@@ -9,6 +9,42 @@ namespace MentorshipProgram.Session3
         [TestMethod]
         public void TestMethod1()
         {
+            var bst = new BinarySearchTree();
+            bst.Insert(10, null);
+        }
+
+        public class BinarySearchTree
+        {
+            public BinaryNode head;
+            public BinarySearchTree()
+            {
+                head = null;
+            }
+            public BinarySearchTree(BinaryNode head)
+            {
+                this.head = head;
+            }
+
+            public void Insert(int val, BinaryNode node)
+            {
+                if (node == null)
+                {
+                    node = new BinaryNode(val);
+                    this.head = node;
+                }
+                else if (val < node.value)
+                {
+                    if (node.left != null)
+                        Insert(val, node.left);
+                    else node.left = new BinaryNode(val);
+                }
+                else
+                {
+                    if (node.right != null)
+                        Insert(val, node.right);
+                    else node.right = new BinaryNode(val);
+                }
+            }
         }
 
         public class BinaryNode
