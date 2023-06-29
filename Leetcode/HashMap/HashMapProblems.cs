ceb0a9a38eaa0ae67e7923ac3f685137dc63f5bf
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-06-29 16:51:13 -0500

    HashMapProblems

---
+++ Leetcode/HashMap/HashMapProblems.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MentorshipProgram.Leetcode.HashMap
+{
+    [TestClass]
+    public class HashMapProblems
+    {
+        [TestMethod]
+        public void NumIdenticalPairs()
+        {
+            int[] nums = { 1, 2, 3, 1, 1, 3 };
+            int result = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+                for (int j = 1; j < nums.Length; j++)
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        Console.Write("(" + i + "," + j + ")");
+                        result++;
+                    }
+
+            Console.WriteLine(result);
+        }
+    }
+}
