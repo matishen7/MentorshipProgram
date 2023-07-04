cfee73af3a10089341738d1942760465a72c84ae
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-07-04 13:12:21 -0500

    FindMatrix

--- Leetcode/HashMap/HashMapProblems.cs
+++ Leetcode/HashMap/HashMapProblems.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MentorshipProgram.Leetcode.HashMap
 {
@@ -44,6 +45,42 @@ namespace MentorshipProgram.Leetcode.HashMap
             }
 
             Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public void FindMatrix()
+        {
+            int[] nums = { 1, 3, 4, 1, 2, 3, 1 };
+            IList<IList<int>> arr = new List<IList<int>>();
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (dic.ContainsKey(nums[i]))
+                {
+                    var count = dic[nums[i]];
+                    dic[nums[i]] = ++count;
+                }
+                else
+                    dic.Add(nums[i], 1);
+
+            }
+
+            while (dic.Count > 0)
+            {
+                var row = new List<int>();
+                for (int i = dic.Count - 1; i >= 0; i--)
+                {
+                    KeyValuePair<int, int> KeyValue = dic.ElementAt(i);
+                    var count = KeyValue.Value;
+                    if (count > 0) row.Add(KeyValue.Key);
+                    count = count - 1;
+                    if (count == 0) dic.Remove(KeyValue.Key);
+                    else dic[KeyValue.Key] = count;
+                }
+                arr.Add(row);
+            }
+            Console.WriteLine(arr);
         }
     }
 }
