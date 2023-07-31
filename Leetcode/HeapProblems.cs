0525ff678a8c6990e0efaf1f44e29ab2dcce6ffe
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-07-31 10:20:45 -0500

    KthFactor

---
+++ Leetcode/HeapProblems.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MentorshipProgram.Session2;
+using static MentorshipProgram.Session2.HeapProgram;
+namespace MentorshipProgram.Leetcode
+{
+    [TestClass]
+    public class HeapProblems
+    {
+        [TestMethod]
+        public void PickGifts()
+        {
+            
+        }
+    }
+}
