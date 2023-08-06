7545f9b6fa1304e6d4e60beaf14d2219d5f0f6c7
Muratbek Ishenbaev <Ishenbaev7@gmail.com>
2023-08-05 22:00:11 -0500

    WordBreak

--- Leetcode/EasyLevel.cs
+++ Leetcode/EasyLevel.cs
@@ -113,5 +113,27 @@ namespace MentorshipProgram.Leetcode
             Array.Reverse(result);
             return result;
         }
+
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            var wordSet = new HashSet<string>(wordDict);
+
+            var dp = new bool[s.Length + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordSet.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.Length];
+        }
     }
 }
